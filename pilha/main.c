8ad6177fbe33e6f09bb24a8eeadefbdf841740f7
JUAN CARLOS RIBEIRO FIUZA <juan.fiuza@a.ucb.br>
2024-05-21 11:16:23 -0300

    add pilha

---
+++ pilha/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+int main(int argc, char* argv[]){
+
+    StackNode* top = NULL;
+
+    push(&top, 'A');
+    push(&top, 'B');
+    push(&top, 'C');
+
+    display(top);
+
+    printf("%c\n", pop(&top));
+
+    display(top);
+
+
+    printf(" %c", pop(&top));
+    printf(" %c", pop(&top));
+    printf(" %c", pop(&top));
+    printf(" %c", pop(&top));
+
+
+
+
+    exit(0);
+}
